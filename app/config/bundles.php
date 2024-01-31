2e064df3739265f85b01f9048e3511da6f385ccb
daniel <dan_kloda@wp.pl>
2024-01-31 16:51:13 +0100

    chore(deps): update symfony to version 7.0

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
