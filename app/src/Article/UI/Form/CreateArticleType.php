80a3adc7b4a2564ae9a32c3d7058048dc74705d3
daniel <dan_kloda@wp.pl>
2024-01-31 18:12:21 +0100

    fix: changes from review

--- app/src/Article/UI/Form/CreateArticleType.php
+++ app/src/Article/UI/Form/CreateArticleType.php
@@ -21,6 +21,7 @@ final class CreateArticleType extends AbstractType
             ->add('save', SubmitType::class)
         ;
     }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
